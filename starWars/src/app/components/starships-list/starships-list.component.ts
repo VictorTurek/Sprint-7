1490842afe15ee385704c35da62f72b6eb9bd88d
VictorTurek <victorturekbcn@gmail.com>
2024-02-06 13:22:13 +0100

    folders and favicon created

---
+++ starWars/src/app/components/starships-list/starships-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starships-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './starships-list.component.html',
+  styleUrl: './starships-list.component.sass'
+})
+export class StarshipsListComponent {
+
+}
