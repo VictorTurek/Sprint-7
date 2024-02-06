ba29c1b18d308a8ab8a13d8fe0385532dfa85ff4
VictorTurek <victorturekbcn@gmail.com>
2024-02-06 13:58:31 +0100

    starting to work on header

--- starWars/src/app/pages/home/home.component.ts
+++ starWars/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { MenuComponent } from '../../components/menu/menu.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
