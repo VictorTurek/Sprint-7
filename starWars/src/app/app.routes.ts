7012ad8b36d80d443a8f981d569417cebab04c47
VictorTurek <victorturekbcn@gmail.com>
2024-02-06 19:28:11 +0100

    navigation bar implemented

--- starWars/src/app/app.routes.ts
+++ starWars/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { StarshipsService } from './services/starships.service';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //{path: '', component: HomeComponent},
+    {path: 'starships', component: StarshipsService}, //routing for the header links. I use :type do define which kind of content we show.
+];
--- starWars/src/app/components/menu/menu.component.ts
+++ starWars/src/app/components/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.sass'
 })
